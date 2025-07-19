0cd7147da9ef3108b0f7348ccc37c7e83a9aa4f4
isaacmatovu <matovuisaac646@gmail.com>
2025-07-19 17:17:33 +0300

    initial commit

---
+++ app/onboading.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const onboading = () => {
+  return (
+    <View>
+      <Text>onboading</Text>
+    </View>
+  );
+};
+
+export default onboading;
+
+const styles = StyleSheet.create({});
